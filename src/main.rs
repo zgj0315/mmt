35abc85d026b4765e63c5ed37ad20bfd9fd55ac3
赵光建 <zhaoguangjian@cyberkl.com>
2022-12-14 23:15:14 +0800

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
+mod lib;
 use std::env;
+use std::path::Path;
 use std::process;
-use std::sync::Arc;
-use std::sync::Mutex;
 
-use mmt::read_file_info_and_copy_file;
-use mmt::read_file_list_and_input_buffer;
+use lib::copy_raw_file;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
+fn main() {
+    let format = tracing_subscriber::fmt::format()
+        .with_level(true)
+        .with_target(false)
+        .with_thread_ids(false)
+        .with_thread_names(false);
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_writer(std::io::stdout)
+        .with_ansi(true)
+        .event_format(format)
+        .init();
     // 处理输入参数，获取输入和输出路径
     let args: Vec<String> = env::args().collect();
-    let src_dir: &String;
-    let dst_dir: &String;
     if args.len() != 3 {
-        println!("arguments error!");
-        println!(
-            "eg: {} /Users/zhaoguangjian/tmp/input /Users/zhaoguangjian/tmp/output",
-            args[0]
-        );
+        log::error!("arguments error!");
+        log::error!("eg: {} ./input ./output", args[0]);
         process::exit(1);
-    } else {
-        src_dir = &args[1];
-        dst_dir = &args[2];
     }
 
-    // 待处理文件列表
-    let file_buffer = Arc::new(Mutex::new(Vec::new()));
-    // 异步方式获取文件列表，写入buffer
-    let future_read = read_file_list_and_input_buffer(src_dir, file_buffer.clone());
-    // 异步方式读取文件信息，copy文件
-    let future_copy = read_file_info_and_copy_file(dst_dir, file_buffer.clone());
-    // join
-    tokio::join!(future_read, future_copy);
+    let input_path = Path::new(&args[1]);
+    let output_path = Path::new(&args[2]);
+    if !input_path.exists() {
+        log::error!("input path {:?} not exists", input_path);
+        process::exit(1);
+    }
+    if !output_path.exists() {
+        log::error!("output path {:?} not exists", output_path);
+        process::exit(1);
+    }
+    copy_raw_file(input_path, output_path);
 }
