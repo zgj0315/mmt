9ba61a3b9eb46381ffb3dd2b4f9b40e8d5cb6643
赵光建 <zhaoguangjian@cyberkl.com>
2022-12-26 13:51:13 +0800

    add bench

--- benches/same_file_benchmark.rs
+++ benches/same_file_benchmark.rs
@@ -1,14 +1,14 @@
 use std::path::Path;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use mmt::multimedia::is_same_file;
+use is_copy::is_file_copy;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let path_a = Path::new("./input/IMG_7705.CR2");
     let path_b = Path::new("./input/IMG_7705_copy.CR2");
 
     c.bench_function("same_file", |b| {
-        b.iter(|| is_same_file(black_box(path_a), black_box(path_b)))
+        b.iter(|| is_file_copy(black_box(path_a), black_box(path_b)))
     });
 }
 
